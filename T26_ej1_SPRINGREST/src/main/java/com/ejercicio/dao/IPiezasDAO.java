a671681c45c6aa7c8f2a0ae18a404608243a3856
Camila Garcia <78553396+camilagarcia36@users.noreply.github.com>
2023-04-27 12:40:29 +0200

    -

--- T26_ej1_SPRINGREST/src/main/java/com/ejercicio/dao/IPiezasDAO.java
+++
@@ -1,9 +0,0 @@
-package com.ejercicio.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.ejercicio.dto.Piezas;
-
-public interface IPiezasDAO extends JpaRepository<Piezas, Long> {
-
-}
