3801bc629aa4a5d66e1a0b9780e3d3cf800681ef
Camila Garcia <78553396+camilagarcia36@users.noreply.github.com>
2023-04-27 11:55:47 +0200

    -

--- T26_ej1_SPRINGREST/src/main/java/com/ejercicio/dao/IProveedoresDAO.java
+++
@@ -1,9 +0,0 @@
-package com.ejercicio.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.ejercicio.dto.Proveedores;
-
-public interface IProveedoresDAO extends JpaRepository<Proveedores, Long> {
-
-}
